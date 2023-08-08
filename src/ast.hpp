a4632ca0ef66779df21abb4547d8b16634ccbd9c
Hiroto Yano <yano.h.ag@m.titech.ac.jp>
2023-08-08 23:42:26 +0900

    ast.hppにコメントの追加

--- src/ast.hpp
+++ src/ast.hpp
@@ -7,34 +7,38 @@
 
 namespace almo {
 
+    // 構文木の属性を表すenum
     enum Type {
-        H1,
-        H2,
-        H3,
-        H4,
-        H5,
-        H6,
-        Block,
-        CodeRunner,
-        CodeBlock,
-        MathBlock,
-        InlineMath,
-        InlineOverline,
-        InlineStrong,
-        InlineItalic,
-        PlainText,
-        InfoBlock,
-        NewLine,
+        H1, // <h1>
+        H2, // <h2>
+        H3, // <h3>
+        H4, // <h4>
+        H5, // <h5>
+        H6, // <h6>
+        Block, // ブロック
+        CodeRunner, // プログラムの実行環境を作る独自記法
+        CodeBlock, // コードブロック
+        MathBlock, // 数式ブロック
+        InlineMath, // インラインの数式ブロック
+        InlineOverline, // インラインの打消し
+        InlineStrong, // インラインの強調
+        InlineItalic, // イタリック
+        PlainText, // 生のテキスト
+        InfoBlock, // infoブロック
+        NewLine, // 改行
     };
 
+    // 構文木のノードを表す構造体
+    // 各ノードにはTypeが付いており、それによってそのノードの役割を判定することができる。
     struct AST {
         using node_ptr = std::shared_ptr<AST>;
         AST() = default;
         AST(Type type, std::string content = "") : type(type), content(content) { }
         Type type;
-        std::string content;
-        std::vector<node_ptr> childs;
+        std::string content; // typeがPlainTextのときのみ使用されます。PlainTextのテキストの中身が格納されます。
+        std::vector<node_ptr> childs; // 構文木の子ノードを保持します。
 
+        // typeがCodeRunnerのときのみ使用されます。CodeRunnerの情報を持ちます。
         std::vector<std::pair<std::string, std::string>> code_runner;
     };
 }
