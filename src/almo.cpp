3ea6d3f5691e3be1028a9ff6f44a6189f8a8604e
abap34 <uchi_ymgc@her0.jp>
2023-09-14 20:09:55 +0900

    標準出力ではなく直接ファイルに書き出すように変更

--- src/almo.cpp
+++ src/almo.cpp
@@ -7,12 +7,23 @@
 int main(int argc, char *argv[]){
     auto [meta_data, asts] = almo::parse_md_file(argv[1]);
     nlohmann::json json_meta_data = almo::make_meta_data_json(meta_data);
+
+
     // if --theme option is specified, use it.
-    if (argc == 4 && std::string(argv[2]) == "--theme"){
+    if (argc > 4 && std::string(argv[2]) == "--theme"){
         json_meta_data["theme"] = argv[3];    
     } else {
         json_meta_data["theme"] = "dark";
     }
+    
     nlohmann::json json_ir = almo::make_json(asts);
-    almo::render(json_ir, json_meta_data);
+
+    // if -o option is specified, use it.
+    if (argc == 6 && std::string(argv[4]) == "-o"){
+        almo::render(json_ir, json_meta_data, argv[5]);
+    } else {
+        almo::render(json_ir, json_meta_data, "index.html");
+    }
+    
+    return 0;
 }
