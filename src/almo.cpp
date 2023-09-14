a4b0f37f44952dc56e5783342848f86c12b1ed35
abap34 <uchi_ymgc@her0.jp>
2023-09-14 21:32:07 +0900

    コマンドライン引数のパーサを拡張可能な実装に変更

--- src/almo.cpp
+++ src/almo.cpp
@@ -7,23 +7,44 @@
 int main(int argc, char *argv[]){
     auto [meta_data, asts] = almo::parse_md_file(argv[1]);
     nlohmann::json json_meta_data = almo::make_meta_data_json(meta_data);
-
-
-    // if --theme option is specified, use it.
-    if (argc > 4 && std::string(argv[2]) == "--theme"){
-        json_meta_data["theme"] = argv[3];    
-    } else {
-        json_meta_data["theme"] = "dark";
-    }
-    
     nlohmann::json json_ir = almo::make_json(asts);
 
-    // if -o option is specified, use it.
-    if (argc == 6 && std::string(argv[4]) == "-o"){
-        almo::render(json_ir, json_meta_data, argv[5]);
-    } else {
-        almo::render(json_ir, json_meta_data, "index.html");
+    std::vector<std::string> args = {"-o", "-t", "-d", "-h"};
+
+    // default
+    std::string theme = "light";
+    std::string out_path = "index.html";
+
+    for (int i = 2; i < argc; i++){
+        if (argv[i][0] == '-'){
+            if (argv[i][1] == 'o'){
+                out_path = argv[i + 1];
+            }
+            else if (argv[i][1] == 't'){
+                theme = argv[i + 1];
+            }
+            else if (argv[i][1] == 'd'){
+                std::cout << json_meta_data.dump(4) << std::endl;
+                std::cout << json_ir.dump(4) << std::endl;
+                return 0;
+            }
+            else if (argv[i][1] == 'h'){
+                if (argc > 3){
+                    std::cerr << "error: -h option is used, but other options are used." << std::endl;
+                    return 1;
+                }
+                std::cout << "usage: almo [options] [input file]" << std::endl;
+                std::cout << "options:" << std::endl;
+                std::cout << "  -o [output file] : output file name" << std::endl;
+                std::cout << "  -t [theme] : theme name" << std::endl;
+                std::cout << "  -d : show debug info" << std::endl;
+                std::cout << "  -h : show this help" << std::endl;
+                return 0;
+            }
+        }
     }
-    
+
+    json_meta_data["theme"] = theme;
+    almo::render(json_ir, json_meta_data, out_path);
     return 0;
 }
