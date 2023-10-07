0d548c3c6c452907d6627e3b293687f41c531f61
abap34 <uchi_ymgc@her0.jp>
2023-10-08 00:55:33 +0900

    `render` のインターフェースを変更

--- src/almo.cpp
+++ src/almo.cpp
@@ -1,35 +1,36 @@
-#include "makejson.hpp"
+// #include "makejson.hpp"
 #include "parse.hpp"
 #include "render.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "utils.hpp"
 
 int main(int argc, char *argv[]){
-    auto [meta_data, asts] = almo::parse_md_file(argv[1]);
-    nlohmann::json json_meta_data = almo::make_meta_data_json(meta_data);
-    nlohmann::json json_ir = almo::make_json(asts);
-
-    // default
-    std::string theme = "light";
-    std::string out_path = argv[1];
-    int dot_pos = out_path.rfind(".");
-    if (dot_pos == std::string::npos){
-        std::cerr << "error: input file name is not valid." << std::endl;
-        return 1;
-    }
-    out_path = out_path.substr(0, dot_pos) + ".html";
-
-    std::string custom_css_path = "";
-
-    bool debug = false;
+    auto [meta_data, ast] = almo::parse_md_file(argv[1]);
     
+    // コマンドライン引数のデフォルト値を設定
+    std::string theme = "light";
+    std::string css_setting = "light";
+    bool debug = false;
+    std::string editor_theme = "ace/theme/xcode";
+    std::string syntax_theme = "github.min";
+
+    // 出力ファイルのデフォルト値を設定。 .md -> .html に置き換える
+    std::string out_path = argv[1];
+    if (out_path.ends_with(".md")){
+        out_path = out_path.substr(0, out_path.length() - 3) + ".html";
+    }
+    else {
+        throw InvalidCommandLineArgumentsError("不正な入力ファイルです。 .md 拡張子を持つファイルを入力してください。");
+        exit(1);
+    }
+
 
     for (int i = 2; i < argc; i++){
         if (argv[i][0] == '-'){
             if (strlen(argv[i]) > 3){
-                std::cerr << "Unkown command line arguments is passed." << std::endl;
-                std::cerr << "Please check the option by `almo -h`" << std::endl;
+                throw InvalidCommandLineArgumentsError("不正なコマンドライン引数です。 -h オプションでヘルプを確認してください。");
                 exit(1);
             }
             if (argv[i][1] == 'o'){
@@ -39,24 +40,30 @@ int main(int argc, char *argv[]){
                 theme = argv[i + 1];
             }
             else if (argv[i][1] == 'c'){
-                custom_css_path = argv[i + 1];
+                css_setting = argv[i + 1];
             }
             else if (argv[i][1] == 'd'){
                 debug = true;
             }
+            else if (argv[i][1] == 'e'){
+                editor_theme = argv[i + 1];
+            }
+            else if (argv[i][1] == 's'){
+                syntax_theme = argv[i + 1];
+            }
             else if (argv[i][1] == 'h'){
                 if (argc > 3){
-                    std::cerr << "error: -h option is used, but other options are used." << std::endl;
-                    return 1;
+                    throw InvalidCommandLineArgumentsError("不正なコマンドライン引数です。 -h オプションと他のオプションは同時に指定できません。");
+                    exit(1);
                 }
-                std::cout << "usage: almo [options] [input file]" << std::endl;
-                std::cout << "options:" << std::endl;
-                std::cout << "  -o [output file] : output file name" << std::endl;
-                std::cout << "  -t [theme] : theme name" << std::endl;
-                std::cout << "  -c [custom css path] : custom style pass" << std::endl;
-                std::cout << "  -d : show debug info" << std::endl;
-                std::cout << "  -h : show this help" << std::endl;
-                return 0;
+                std::cout << "使用法: almo <入力> [オプション]" << std::endl;
+                std::cout << "オプション:" << std::endl;
+                std::cout << "  -o <出力>     出力ファイル名を指定します。デフォルトは入力ファイル名の拡張子を .html に変更したものです。" << std::endl;
+                std::cout << "  -t <テーマ>   テーマを指定します。デフォルトは light です。" << std::endl;
+                std::cout << "  -c <CSS>      CSSファイルを指定します。デフォルトは テーマに付属するものが使用されます。" << std::endl;
+                std::cout << "  -e <テーマ>   エディタのテーマを指定します。デフォルトは  です。" << std::endl;
+                std::cout << "  -d            デバッグモードで実行します。" << std::endl;
+                std::cout << "  -h            ヘルプを表示します。" << std::endl;
             }
             else {
                 std::cerr << "Unkown command line arguments is passed." << std::endl;
@@ -66,17 +73,27 @@ int main(int argc, char *argv[]){
         }
     }
 
-    json_meta_data["theme"] = theme;
-    json_meta_data["out_path"] = out_path;
-    json_meta_data["custom_css_path"] = custom_css_path;
 
-    if (debug) {
-        nlohmann::json all_data;
-        all_data["meta"] = json_meta_data;
-        all_data["ir"] = json_ir;
-        std::cout << all_data.dump(4) << std::endl;
+    // もし theme が dark であれば、 editor_theme と syntax_theme も対応したものにしておく
+    if (theme == "dark"){
+        editor_theme = "ace/theme/monokai";
+        syntax_theme = "monokai-sublime.min";
     }
 
-    almo::render(json_ir, json_meta_data, out_path);
+    if (debug) {
+        nlohmann::json ast_json = ast.to_json();
+        std::cout << ast_json.dump(4) << std::endl;
+    }
+
+    // コマンドライン引数を meta_data に追加
+    meta_data["theme"] = theme;
+    meta_data["out_path"] = out_path;
+    meta_data["css_setting"] = css_setting;
+    meta_data["editor_theme"] = editor_theme;
+    meta_data["syntax_theme"] = syntax_theme;
+
+    almo::meta_data = meta_data;
+
+    almo::render(ast, meta_data, out_path);
     return 0;
 }
