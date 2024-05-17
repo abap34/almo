1964b5ebff9a8fc5b02deb3c67a111fb26df02f2
abap34 <abap0002@gmail.com>
2024-05-18 00:49:51 +0900

    `md_to_json`, `md_to_json` をバインド

--- src/pyalmo.cpp
+++ src/pyalmo.cpp
@@ -9,10 +9,26 @@ std::string md_to_html(std::string md_content, std::map<std::string, std::string
     return ast.render(meta_data);
 }
 
+
+std::string md_to_json(std::string md_content) {
+    almo::Block ast = almo::parse_rest(split(md_content, "\n"));
+    return ast.to_json();
+}
+
+
+std::string md_to_dot(std::string md_content) {
+    almo::Block ast = almo::parse_rest(split(md_content, "\n"));
+    return ast.to_dot(true);
+}
+
+
 namespace py = pybind11;
 
+
 PYBIND11_MODULE(almo, m) {
-    m.doc() = "almo library"; 
+    m.doc() = "almo interface for python.";
 
     m.def("md_to_html", &md_to_html, "md to html");
+    m.def("md_to_json", &md_to_json, "md to json");
+    m.def("md_to_dot", &md_to_dot, "md to dot");
 }
