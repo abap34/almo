4f7f1c113b32a0d05e88b15b2aa4cbe685af5ff1
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-05-18 04:08:34 +0900

    new_* を削除

--- src/new_syntax_all.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include"syntax/Header.hpp"
-#include"syntax/NewLine.hpp"
-#include"syntax/RawText.hpp"
-#include"syntax/InlineMath.hpp"
-#include"syntax/EOF.hpp"
-#include"syntax/Markdown.hpp"
