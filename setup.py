c5b48ac7a63769323bf592c9747da8fe17a3730f
abap34 <abap0002@gmail.com>
2024-05-18 17:31:45 +0900

    `long_description` を追加

--- setup.py
+++ setup.py
@@ -14,10 +14,14 @@ ext_modules = [
     ),
 ]
 
+long_description = open("README.md").read()
+
 setup(
     name="almo",
     author=["abap34", "ebi-fly13", "noya2"],
     url="https://github.com/abap34/ALMO",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
     requires=["pybind11"],
