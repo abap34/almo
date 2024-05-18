fd5680e0b31c0da2b8d0000002df0d150a0d764d
abap34 <abap0002@gmail.com>
2024-05-18 23:32:50 +0900

    バージョンを自動でタグに合わせるように修正

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 os.system("bash scripts/setup.sh")
-__version__ = "0.8.4"
+__version__ = os.getenv("VERSION")
 
 ext_modules = [
     Pybind11Extension(
