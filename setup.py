2c1cadad72baff76d2bc8fc87846909843db1088
abap34 <abap0002@gmail.com>
2024-05-18 18:42:28 +0900

    author の設定を正しくした

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = open("README.md").read()
 
 setup(
     name="almo",
-    author=["abap34", "ebi-fly13", "noya2"],
+    author="abap34, ebi-fly13, noya2",
     url="https://github.com/abap34/ALMO",
     long_description_content_type="text/markdown",
     long_description=long_description,
