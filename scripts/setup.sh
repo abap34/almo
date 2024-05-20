e437219c611d41b4c0c18340adda7ff89795cfcb
abap34 <abap0002@gmail.com>
2024-05-20 15:08:47 +0900

    コンパイル時に埋め込むファイルを指定できるようにした

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,31 +1,61 @@
 #!/bin/bash
 
 src_dir="src/"
-
 build_dir="build/"
+data_petterns=("*.css" "*.html" "*.js")
+verbose=0
 
-mkdir -p "$build_dir"
 
-for file in "$src_dir"*
-do
-    name="$(basename "$file")"
-    extension="${name##*.}"
-    if [ "$extension" != "cpp" ] && [ "$extension" != "hpp" ]; then
-        contents="$(cat "$file")"
-        escaped_contents="${contents//\\/\\\\}" 
-        echo -e "R\"(\n" > "$build_dir"$name
-        echo -e "$escaped_contents" >> "$build_dir"$name
-        echo -e "\n)\"" >> "$build_dir"$name
-        echo "ファイル $file を処理済み"
-    else
-        cp "$file" "$build_dir"
-        echo "ファイル $file をコピー"
+function vprint() {
+    if [ $verbose -eq 1 ]; then
+        echo "$1"
     fi
+}
+
+while getopts "v" opt; do
+    case $opt in
+        v)
+            verbose=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
 done
 
 
+vprint "========================================"
+vprint "Start setup"
+vprint "    src_dir: $src_dir"
+vprint "    build_dir: $build_dir"
+vprint "    data_petterns: ${data_petterns[@]}"
+vprint "========================================"
 
 
+mkdir -p "$build_dir"
+cp -r "$src_dir" "$build_dir"
 
 
+# ワイルドカードが展開されるとカレントディレクトリのファイル名で展開されてマッチがおかしくなるので展開を抑制する.
+# たとえばカレントディレクトリに `tmp.html` があると pettern が "tmp.html" と展開されてマッチしてしまうので抑制する必要がある。
+set -f
+
+for file in $(find $build_dir -type f); do  
+    vprint "----------------------------------------"
+    vprint "Check $file for data files"
+    for pettern in ${data_petterns[@]}; do
+        vprint  "   Check $pettern"
+        if [[ $file == $build_dir/*$pettern ]]; then
+            echo "$file is matching $pettern"
+            echo "R\"(" > "$file.tmp"
+            cat "$file" >> "$file.tmp"
+            echo ")\"" >> "$file.tmp"
+            mv "$file.tmp" "$file"
+            continue 2
+        fi
+    done
+
+    vprint "$file is not matching any data file"
+done
 
